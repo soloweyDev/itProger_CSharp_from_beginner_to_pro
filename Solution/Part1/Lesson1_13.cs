8b12198fa3baaa2c6cdaaeb2c6c7aa4c44bf90d3
soloweyDev <solowey@narod.ru>
2019-05-04 19:10:11 +0300

    13 урок

---
+++ Solution/Part1/Lesson1_13.cs
@@ -0,0 +1,32 @@
+﻿namespace Part1
+{
+    /*
+     Создайте структуру, которая будет описывать характеристики компьютера: процессор, вес, время автономной работы, количество ядер, установленная ОС.
+     На основе такой структуры создайте объект и присвойте ему пару значений.
+     */
+    struct Computer
+    {
+        string proc;
+        float weight;
+        float timeWork;
+        short numberCores;
+        string os;
+
+        public Computer(string proc, float weight, float timeWork, short numberCores, string os)
+        {
+            this.proc = proc;
+            this.weight = weight;
+            this.timeWork = timeWork;
+            this.numberCores = numberCores;
+            this.os = os;
+        }
+    }
+    class Lesson1_13
+    {
+        void Test()
+        {
+            Computer computer1 = new Computer("AMD", 15.5f, 0.0f, 6, "Windows");
+            Computer computer2 = new Computer("Intel", 12.5f, 1.1f, 2, "MacOS");
+        }
+    }
+}
